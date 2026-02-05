45143bc004bfb343264a4691a032ca01c9db94e9
mike_banner <mike.webfree@gmail.com>
2026-02-05 22:30:13 +0100

    fix:  link/wordpress-astro

--- src/lib/woocommerce.js
+++ src/lib/woocommerce.js
@@ -56,6 +56,9 @@ function mapProduct(product) {
 
         /* Catégorie principale */
         category: product.categories?.[0]?.slug || "",
+
+        /* Lien direct WordPress */
+        permalink: product.permalink || "",
     };
 }
 
